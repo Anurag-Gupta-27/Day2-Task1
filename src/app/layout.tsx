20a9ff1bfd145bfde48ccf4d2cb933b75f66d901
Anurag Gupta <143582656+Anurag-Gupta-27@users.noreply.github.com>
2024-10-22 11:02:55 +0530

    removed un-used code

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,4 @@
-import { ClerkProvider, SignInButton, SignedIn, SignedOut, UserButton } from '@clerk/nextjs'
+import { ClerkProvider } from '@clerk/nextjs'
 import './globals.css'
 import { Inter } from 'next/font/google'
 import Header from '../components/header'
